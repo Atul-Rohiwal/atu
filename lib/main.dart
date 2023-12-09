e0479866cd0b0ec274d1dcadd74ea7815ea18bd5
ATUL <124764308+Atul-Rohiwal@users.noreply.github.com>
2023-12-09 21:38:15 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,108 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    home: Home(),
+      home: CWPage("1", 5, 5), // Adjust parameters accordingly
     );
   }
 }
-class Home extends StatelessWidget {
-  const Home({super.key});
+
+class CWPage extends StatefulWidget {
+  final String thisCW;
+  final int rows;
+  final int cols;
+
+  CWPage(this.thisCW, this.rows, this.cols);
+
+  @override
+  _CWPageState createState() => _CWPageState(thisCW, rows, cols);
+}
+
+class _CWPageState extends State<CWPage> {
+  final String thisCW;
+  final int rows;
+  final int cols;
+
+  _CWPageState(this.thisCW, this.rows, this.cols);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("helo world"),
+        title: Text('Crossword #$thisCW'),
       ),
+      body: Center(
+        child: CrosswordPuzzle(
+          rows: rows,
+          cols: cols,
+          words: [
+            CrosswordWord("example", 0, 0, true),
+            CrosswordWord("flutter", 1, 1, false),
+            // Add more words as needed
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CrosswordWord {
+  final String word;
+  final int startX;
+  final int startY;
+  final bool isVertical;
+
+  CrosswordWord(this.word, this.startX, this.startY, this.isVertical);
+}
+
+class CrosswordPuzzle extends StatelessWidget {
+  final int rows;
+  final int cols;
+  final List<CrosswordWord> words;
+
+  CrosswordPuzzle({required this.rows, required this.cols, required this.words});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: cols,
+      ),
+      itemCount: rows * cols,
+      itemBuilder: (context, index) {
+        // Check if the current cell is part of any word
+        var word = words.firstWhere(
+              (word) =>
+          (word.isVertical &&
+              word.startX == index % cols &&
+              word.startY <= index / cols && // Check if within word range
+              index / cols < word.startY + word.word.length) ||
+              (!word.isVertical &&
+                  word.startY == index / cols &&
+                  word.startX <= index % cols && // Check if within word range
+                  index % cols < word.startX + word.word.length),
+          orElse: () => CrosswordWord("", 0, 0, false),
+        );
+
+        return Container(
+          decoration: BoxDecoration(
+            border: Border.all(),
+            color: word.word.isNotEmpty ? Colors.yellow : Colors.white,
+          ),
+          child: Center(
+            child: Text(
+              word.word.isNotEmpty ? word.word[0] : '',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        );
+      },
     );
   }
 }
